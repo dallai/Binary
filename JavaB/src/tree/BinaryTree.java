3dcc876bbd1d8f46a06134aa400aaa2fd43775d2
user <user@user-PC>
2015-05-15 08:59:54 +0900

    first Commit

---
+++ JavaB/src/tree/BinaryTree.java
@@ -0,0 +1,270 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Node
+{
+	public int data;
+	public Node left;
+	public Node right;
+	public Node parent;
+	
+	public Node(int ddata, Node left, Node right)
+	{
+	    this.data = ddata;
+	    this.left = left;
+	    this.right =right;    
+	    this.parent = null;
+	}
+	
+	public void displayNode(Node n)
+	{
+	    System.out.println(n.data +" ");  
+	}
+	public void displayNode()
+	{
+	    System.out.println(data +" ");  
+	}
+}
+
+class BinaryTree
+{
+	Node root;
+	
+	public BinaryTree()
+	{
+	    this.root = null;
+	}
+	
+	public Node insertLeft(int parent,int leftvalue )
+	{
+		Node n = find(root,parent);
+	    Node leftchild = new Node(leftvalue, null, null);
+	    n.left = leftchild;
+	    leftchild.parent = n;
+	    return leftchild;
+	}
+	
+	public Node insertRight(int parent, int rightvalue)
+	{
+	    Node n = find(root,parent);
+	    Node rightchild = new Node(rightvalue, null, null);
+	    n.right = rightchild;
+	    rightchild.parent = n;
+	    return rightchild;
+	}
+	
+	public void insertRoot(int data)
+	{
+	    root = new Node(data, null, null);
+	}
+	
+	public Node getRoot()
+	{
+	    return root;
+	}
+	
+	public Node find(Node n,int key)
+	{       
+	    Node result = null;
+	    if (n == null)
+	        return null;
+	    if (n.data ==key)
+	        return n;
+	    if (n.left != null)
+	        result = find(n.left,key);
+	    if (result == null)
+	        result = find(n.right,key);
+	    return result;
+	} 
+	
+	public int getheight(Node root)
+	{
+	    if(root == null)
+	        return  0;      
+	    return Math.max(getheight(root.left),getheight(root.right))+1; 
+	}
+	
+	public static void printTree(Node n)
+	{       
+	    if( n == null)
+	        return;
+	    
+
+	    printTree(n.left);
+	    n.displayNode(n);
+	    printTree(n.right);             
+	}
+	
+	public static void cloneTree(BinaryTree source, BinaryTree target) {
+		
+		Node clonePoint = source.find(source.root, target.root.data);
+		if(target.root.left.data == clonePoint.parent.data) {
+			clonePoint.left = target.root.right;
+		} else {
+			clonePoint.right = target.root.left;
+		}
+	}
+	
+	public static void rotateTree(BinaryTree btree, BinaryTree resTree, Node sourceMe, Node targetMe) {
+        
+	//	int input = 0;
+		
+		if(sourceMe == null || targetMe == null) return;
+		if(targetMe.data == btree.root.data) return;
+		
+		Node childLeft = null;
+		Node childRight = null;
+		Node resLeftChild = null;
+		Node resRightChild = null;
+		
+		List<Node> array = new ArrayList<Node>();
+		
+	    Node parent = sourceMe.parent;
+	    childLeft = sourceMe.left;
+	    childRight = sourceMe.right;
+
+	    if(parent != null) {
+	    	array.add(parent);
+	    }
+	    if(childLeft != null) {
+	    	array.add(childLeft);
+	    }
+	    if(childRight != null) {
+	    	array.add(childRight);
+	    }
+
+	    for(int i = 0 ; i < array.size() ; i++) {
+	    	if( targetMe.parent != null && array.get(i).data == targetMe.parent.data)
+	    		array.remove(i);
+	    }
+	    
+	    if(array.size() == 1)
+			resLeftChild = resTree.insertLeft(sourceMe.data, array.get(0).data);
+		if(array.size() == 2){
+			resLeftChild = resTree.insertLeft(sourceMe.data, array.get(0).data);
+			resRightChild = resTree.insertRight(sourceMe.data, array.get(1).data);}
+			
+		if(array.size() == 1)
+			rotateTree(btree,resTree,array.get(0), resLeftChild);
+		if(array.size() == 2) {
+			rotateTree(btree,resTree,array.get(0), resLeftChild);
+			rotateTree(btree,resTree,array.get(1), resRightChild);
+		}
+	}
+	
+	public static void main(String args[]) {
+		BinaryTree btree = new BinaryTree();
+		BinaryTree resBtree= new BinaryTree();
+		
+		int pickNumber = 4;
+		btree.insertRoot(100);
+		btree.insertLeft(100, 2);
+		btree.insertRight(100, 3);
+		btree.insertLeft(2, 4);
+		btree.insertRight(2, 5);
+		btree.insertLeft(3, 6);
+		btree.insertRight(3, 7);
+		btree.insertRight(6, 8);
+		btree.insertLeft(5, 9);
+		btree.insertRight(5, 10);
+		btree.insertRight(10, 11);
+
+		Node me = btree.find(btree.root, pickNumber);
+		resBtree.insertRoot(me.data);
+		
+		rotateTree(btree,resBtree,me, resBtree.root);
+		cloneTree(resBtree, btree);
+	
+		BTreePrinter.printNode(btree.root);
+		BTreePrinter.printNode(resBtree.root);
+	}
+}
+
+class BTreePrinter {
+
+    public static  void printNode(Node root) {
+        int maxLevel = BTreePrinter.maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static  void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        BTreePrinter.printWhitespaces(firstSpaces);
+
+        List<Node> newNodes = new ArrayList<Node>();
+        for (Node node : nodes) {
+            if (node != null) {
+                System.out.print(node.data);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            BTreePrinter.printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                BTreePrinter.printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static  int maxLevel(Node node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+    }
+
+    private static  boolean isAllElementsNull(List<Node> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+    }
+}
